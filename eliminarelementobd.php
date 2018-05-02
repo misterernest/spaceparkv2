9d52cfe66c09d87c344cf7dbf84ca6c4c2017183
J. Jonatan Rojas R <jjrrdecali90@gmail.com>
2018-05-02 13:18:08 -0500

    se crea la contra consulta del delete con purebas y funcionalidad y guardando en la tabla cache

--- eliminarelementobd.php
+++ eliminarelementobd.php
@@ -8,11 +8,50 @@ if (isset($_POST['id']) && !empty($_POST['id']))  {
 	require_once 'config.php';	// consultamos la reserva
 	// Datos recibidos
 	$id = $_POST['id'];
+	
+
+	// empieza deshacer
+	$sql="SELECT * FROM area_ocupada WHERE id=:id";
+	$prepared = $pdo->prepare($sql);
+	$resulti = $prepared->execute(['id'=>$id]);
+    $resulti = $prepared->fetch(PDO::FETCH_ASSOC);
+    
+	$coordenada_x = $resulti['coordenada_x'];
+	$coordenada_y = $resulti['coordenada_y'];
+	$ancho_x = $resulti['ancho_x'];
+	$largo_y = $resulti['largo_y'];
+	$fecha_incial = $resulti['fecha_incial'];
+	$fecha_final = $resulti['fecha_final'];
+	$categoria = $resulti['categoria'];
+	$cliente = $resulti['cliente'];
+	$angulo = $resulti['angulo'];
+	$comentario = $resulti['comentario'];
+
+	
+	$contraquery="INSERT INTO area_ocupada VALUES (
+		$id,
+		$coordenada_x,
+		$coordenada_y, 
+		$ancho_x,
+		$largo_y, 
+		\'$fecha_incial\', 
+		\'$fecha_final\', 
+		\'$categoria\', 
+		\'$cliente\', 
+		$angulo, 
+		\'$comentario\'
+	); ";
+
+    $sql="INSERT INTO cache VALUES (NULL, '$contraquery')";
+	$prepared = $pdo->prepare($sql);
+	$resultC = $prepared->execute();
+	$prepared = NULL;
+	//termina el deshacer
 
 	$queryd = "DELETE FROM area_ocupada WHERE id ='".$_POST['id']."'";
 	$prepared = $pdo->prepare($queryd);
 	$resulti = $prepared->execute();
-	/* $prepared = null; */
+	$prepared = null;
 
 
 
